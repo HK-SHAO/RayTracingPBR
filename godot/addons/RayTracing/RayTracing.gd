83b4d2b6df8fdd6623681eb30f63dded594a30c8
HK-SHAO <hk-shao@foxmail.com>
2022-11-15 05:02:19 +0800

    update project

---
+++ godot/addons/RayTracing/RayTracing.gd
@@ -0,0 +1,39 @@
+@tool
+extends EditorPlugin
+
+const MainPanel = preload("res://addons/RayTracing/main_panel.tscn")
+
+var main_panel_instance: Control
+
+func _enter_tree() -> void:
+	main_panel_instance = MainPanel.instantiate()
+	# Add the main panel to the editor's main viewport.
+	get_editor_interface().get_editor_main_screen().add_child(main_panel_instance)
+	# Hide the main panel. Very much required.
+	_make_visible(false)
+
+
+func _exit_tree() -> void:
+	if is_instance_valid(main_panel_instance):
+		main_panel_instance.queue_free()
+
+func _has_main_screen():
+	return true
+
+
+func _make_visible(visible):
+	if is_instance_valid(main_panel_instance):
+		main_panel_instance.visible = visible
+		if visible:
+			main_panel_instance.process_mode = Node.PROCESS_MODE_INHERIT
+		else:
+			main_panel_instance.process_mode = Node.PROCESS_MODE_DISABLED
+
+
+func _get_plugin_name():
+	return "RayTracing"
+
+
+func _get_plugin_icon():
+	return get_editor_interface().get_base_control().get_theme_icon("3D", "EditorIcons")
+
---
+++ godot/addons/RayTracing/shared/script/always/alwasy_fill_viewport.gd
@@ -0,0 +1,10 @@
+@tool
+
+extends Node
+
+@onready var control: Control = $".."
+@onready var viewport: SubViewport = $"../.."
+
+
+func _process(_delta):
+	control.size = viewport.size
