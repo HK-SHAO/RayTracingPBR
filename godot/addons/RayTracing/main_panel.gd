9ac0a41665f3b89750643f8276fd434d9a8dd0a2
HK-SHAO <hk-shao@foxmail.com>
2022-11-24 16:48:57 +0800

    add fixed fps

--- godot/addons/RayTracing/main_panel.gd
+++ godot/addons/RayTracing/main_panel.gd
@@ -7,6 +7,9 @@ extends Control
 var initCameraTransform: Transform3D
 var initCameraRotation: Vector3
 
+var fps: int = 0
+var fixed_fps: int = 90
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
     initCameraTransform = camera.transform
@@ -16,26 +19,51 @@ func _ready() -> void:
     %camera_fov_s.value = camera.fov
     %camera_focus_s.value = %always_uniform_camera.focus
     %gamma_s.value = %always_uniform_camera.gamma
-    %max_sample_s.value = %OutShaderRect.max_sample
+    %max_sample_s.value = %PostProcessShader.max_sample
+    %light_quality_s.value = %always_uniform_camera.quality
+    %fixed_fps_edit.text = str(fixed_fps)
 
+var delta_fps_cache: Array = []
+var last_average_fps = 0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _fixed_fps(delta: float) -> void:
+    var delta_fps := fps - fixed_fps
+#    print(delta_fps)
+#    print(delta_fps_cache)
+    if delta_fps_cache.size() >= 100: delta_fps_cache.pop_front()
+    delta_fps_cache.append(delta_fps)
+    
+    var average_fps = 0
+    for i in delta_fps_cache:
+        average_fps += i
+    average_fps /= delta_fps_cache.size()
+    
+#    print(average_fps)
+    var base = %light_quality_s.value
+    if average_fps >= 0:    base += 1 + abs(average_fps)
+    else:                   base -= 0.001 + abs(average_fps) * 0.001
+    
+    %light_quality_s.value = base
+
 func _process(delta: float) -> void:
-    %fps.text = str(Performance.get_monitor(Performance.TIME_FPS))
-    %sample.text = str(%OutShaderRect.frame)
+    fps = Performance.get_monitor(Performance.TIME_FPS)
+    %fps.text = str(fps)
+    %sample.text = str(%PostProcessShader.frame)
     if is_instance_valid(camera):
         %camera_speed.text = str(camera.max_speed)
         %camera_speed_s.value = camera.max_speed
+        
+    _fixed_fps(delta)
 
 
 func _on_sampleonceb_pressed() -> void:
-    %OutShaderRect.frame = 0
+    %PostProcessShader.frame = 0
 
 
 func _on_restcamerab_pressed() -> void:
     camera.transform = initCameraTransform
     camera.reset_rotation(initCameraRotation)
-    %OutShaderRect.frame = 0
+    %PostProcessShader.frame = 0
 
 
 func _on_camera_speed_s_value_changed(value: float) -> void:
@@ -45,27 +73,40 @@ func _on_camera_speed_s_value_changed(value: float) -> void:
 func _on_camera_aperture_s_value_changed(value: float) -> void:
     %always_uniform_camera.aperture = value
     %camera_aperture.text = str(value)
-    %OutShaderRect.frame = 0
+    %PostProcessShader.frame = 0
 
 
 func _on_camera_focus_s_value_changed(value: float) -> void:
     %always_uniform_camera.focus = value
     %camera_focus.text = str(value)
-    %OutShaderRect.frame = 0
+    %PostProcessShader.frame = 0
 
 
 func _on_camera_fov_s_value_changed(value: float) -> void:
     %always_uniform_camera.vfov = value
     %camera_fov.text = str(value)
-    %OutShaderRect.frame = 0
+    %PostProcessShader.frame = 0
 
 
 func _on_gamma_s_value_changed(value: float) -> void:
     %always_uniform_camera.gamma = value
     %gamma.text = str(value)
-    %OutShaderRect.frame = 0
+    %PostProcessShader.frame = 0
 
 
 func _on_max_sample_s_value_changed(value: float) -> void:
-    %OutShaderRect.max_sample = int(value)
+    %PostProcessShader.max_sample = int(value)
     %max_sample.text = str(value)
+
+
+func _on_light_quality_s_value_changed(value: float) -> void:
+    %always_uniform_camera.quality = value
+    %light_quality.text = str(value)
+    # %PostProcessShader.frame = 0
+
+func _on_fixed_fps_edit_text_changed() -> void:
+    var text: String = %fixed_fps_edit.text
+    var value: int = clampi(abs(int(text)), 1, 360)
+    var str_value = str(value)
+    if str_value != text:
+        %fixed_fps_edit.text = str_value
--- godot/addons/RayTracing/shared/script/always/always_uniform_camera.gd
+++ godot/addons/RayTracing/shared/script/always/always_uniform_camera.gd
@@ -7,14 +7,19 @@ extends Node
 var material: ShaderMaterial
 var control: Control
 
+var post_process_material: ShaderMaterial
+
 var gamma: float = 0.5
 var focus: float = 2.0
 var aperture: float = 0.005
 var vfov: float = 30
+var quality: float = 5
 
 func _ready() -> void:
-    control = get_parent() as Control
+    control = %ShaderRect as Control
     material = get_parent().material
+    
+    post_process_material = %PostProcessShader.material
 
 
 func _process(_delta: float) -> void:
@@ -28,6 +33,8 @@ func _process(_delta: float) -> void:
     material.set_shader_parameter("camera_rotation", camera_rotation)
     material.set_shader_parameter("camera_aspect", aspect)
     material.set_shader_parameter("camera_vfov", vfov)
-    material.set_shader_parameter("camera_gamma", gamma)
     material.set_shader_parameter("camera_focus", focus)
     material.set_shader_parameter("camera_aperture", aperture)
+    material.set_shader_parameter("camera_gamma", gamma)
+    
+    material.set_shader_parameter("light_quality", 1 / quality)
