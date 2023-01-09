5759a9c038e728788031c7254834b86fd0e7643a
jiayewang(王家晔) <jiayewang@tencent.com>
2023-01-09 11:48:49 +0800

    dev

--- godot/addons/RayTracing/shared/script/always/alwasy_fill_viewport.gd
+++
@@ -1,9 +0,0 @@
-@tool
-
-extends Node
-
-@onready var control: Control = $".."
-@onready var viewport: SubViewport = $"../.."
-
-func _process(_delta: float) -> void:
-    control.size = viewport.size
