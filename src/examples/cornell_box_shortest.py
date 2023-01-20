8d1fe77bbe43b9bf6c0c4d84cc0ca3978d07b2e9
HK-SHAO <hk-shao@foxmail.com>
2023-01-20 23:00:54 +0800

    update

--- src/examples/cornell_box_shortest.py
+++ src/examples/cornell_box_shortest.py
@@ -7,25 +7,36 @@ image_resolution = (512, 512)
 image_buffer = ti.Vector.field(4, float, image_resolution)
 image_pixels = ti.Vector.field(3, float, image_resolution)
 SCREEN_PIXEL_SIZE = 1.0 / vec2(image_resolution)
-SAMPLE_PER_PIXEL = 1
-MIN_DIS      = 0.05
-MAX_DIS      = 2000.0
-PRECISION    = 0.001
-VISIBILITY   = 0.000001
-MAX_RAYMARCH = 512
-MAX_RAYTRACE = 3
-ENV_IOR = 1.000277
 aspect_ratio    = image_resolution[0] / image_resolution[1]
-light_quality   = 128.0
-camera_exposure = 1
-camera_vfov     = 35
-camera_gamma    = 2.2
 
 Ray = ti.types.struct(origin=vec3, direction=vec3, color=vec3)
 Material = ti.types.struct(albedo=vec3, emission=vec3)
 Transform = ti.types.struct(position=vec3, rotation=vec3, scale=vec3)
 SDFObject = ti.types.struct(distance=float, transform=Transform, material=Material)
 HitRecord = ti.types.struct(object=SDFObject, position=vec3, distance=float, hit=bool)
+
+OBJECT_LIST = [
+    SDFObject(transform=Transform(vec3(0, 0, -1), vec3(0, 0, 0), vec3(1, 1, 0.2)),
+                material=Material(vec3(1, 1, 1)*0.4, vec3(1))),
+    SDFObject(transform=Transform(vec3(0, 1, 0), vec3(90, 0, 0), vec3(1, 1, 0.2)),
+                material=Material(vec3(1, 1, 1)*0.4, vec3(1))),
+    SDFObject(transform=Transform(vec3(0, -1, 0), vec3(90, 0, 0), vec3(1, 1, 0.2)),
+                material=Material(vec3(1, 1, 1)*0.4, vec3(1))),
+    SDFObject(transform=Transform(vec3(-1, 0, 0), vec3(0, 90, 0), vec3(1, 1, 0.2)),
+                material=Material(vec3(1, 0, 0)*0.5, vec3(1))),
+    SDFObject(transform=Transform(vec3(1, 0, 0), vec3(0, 90, 0), vec3(1, 1, 0.2)),
+                material=Material(vec3(0, 1, 0)*0.5, vec3(1))),
+    SDFObject(transform=Transform(vec3(-0.275, -0.3, -0.2), vec3(0, 112, 0), vec3(0.25, 0.5, 0.25)),
+                material=Material(vec3(1, 1, 1)*0.4, vec3(1))),
+    SDFObject(transform=Transform(vec3(0.275, -0.55, 0.2), vec3(0, -197, 0), vec3(0.25, 0.25, 0.25)),
+                material=Material(vec3(1, 1, 1)*0.4, vec3(1))),
+    SDFObject(transform=Transform(vec3(0, 0.809, 0), vec3(90, 0, 0), vec3(0.2, 0.2, 0.01)),
+                material=Material(vec3(1, 1, 1), vec3(100)))
+]
+
+objects_num = len(OBJECT_LIST)
+objects = SDFObject.field(shape=objects_num)
+for i in range(objects_num): objects[i] = OBJECT_LIST[i]
 
 @ti.func
 def angle(a: vec3) -> mat3:
@@ -36,32 +47,9 @@ def angle(a: vec3) -> mat3:
 
 @ti.func
 def signed_distance(obj: SDFObject, pos: vec3) -> float:
-    p = angle(radians(obj.transform.rotation)) @ (pos - obj.transform.position * 10)
-    q = abs(p) - obj.transform.scale * 10
-    return length(max(q, 0)) + min(max(q.x, max(q.y, q.z)), 0) - 0.01
-
-WORLD_LIST = [
-    SDFObject(  transform=Transform(vec3(0, 0, -1), vec3(0, 0, 0), vec3(1, 1, 0.2)),
-                material=Material(vec3(1, 1, 1)*0.4, vec3(1))),
-    SDFObject(  transform=Transform(vec3(0, 1, 0), vec3(90, 0, 0), vec3(1, 1, 0.2)),
-                material=Material(vec3(1, 1, 1)*0.4, vec3(1))),
-    SDFObject(  transform=Transform(vec3(0, -1, 0), vec3(90, 0, 0), vec3(1, 1, 0.2)),
-                material=Material(vec3(1, 1, 1)*0.4, vec3(1))),
-    SDFObject(  transform=Transform(vec3(-1, 0, 0), vec3(0, 90, 0), vec3(1, 1, 0.2)),
-                material=Material(vec3(1, 0, 0)*0.5, vec3(1))),
-    SDFObject(  transform=Transform(vec3(1, 0, 0), vec3(0, 90, 0), vec3(1, 1, 0.2)),
-                material=Material(vec3(0, 1, 0)*0.5, vec3(1))),
-    SDFObject(  transform=Transform(vec3(-0.275, -0.3, -0.2), vec3(0, -253, 0), vec3(0.25, 0.5, 0.25)),
-                material=Material(vec3(1, 1, 1)*0.4, vec3(1))),
-    SDFObject(  transform=Transform(vec3(0.275, -0.55, 0.2), vec3(0, -197, 0), vec3(0.25, 0.25, 0.25)),
-                material=Material(vec3(1, 1, 1)*0.4, vec3(1))),
-    SDFObject(  transform=Transform(vec3(0, 0.809, 0), vec3(90, 0, 0), vec3(0.2, 0.2, 0.01)),
-                material=Material(vec3(1, 1, 1), vec3(100)))
-]
-
-objects_num = len(WORLD_LIST)
-objects = SDFObject.field(shape=objects_num)
-for i in range(objects_num): objects[i] = WORLD_LIST[i]
+    p = angle(radians(obj.transform.rotation)) @ (pos - obj.transform.position)
+    q = abs(p) - obj.transform.scale
+    return length(max(q, 0)) + min(max(q.x, max(q.y, q.z)), 0) - 0.001
 
 @ti.func
 def nearest_object(p: vec3) -> SDFObject:
@@ -73,7 +61,7 @@ def nearest_object(p: vec3) -> SDFObject:
 
 @ti.func
 def calc_normal(obj: SDFObject, p: vec3) -> vec3:
-    e = vec2(1, -1) * PRECISION
+    e = vec2(1, -1) * 0.001
     return normalize(e.xyy * signed_distance(obj, p + e.xyy) + \
                      e.yyx * signed_distance(obj, p + e.yyx) + \
                      e.yxy * signed_distance(obj, p + e.yxy) + \
@@ -81,13 +69,13 @@ def calc_normal(obj: SDFObject, p: vec3) -> vec3:
 
 @ti.func
 def raycast(ray: Ray) -> HitRecord:
-    record = HitRecord(distance=MIN_DIS)
-    for _ in range(MAX_RAYMARCH):
+    record = HitRecord(distance=0.001)
+    for _ in range(256):
         record.position  = ray.origin + record.distance * ray.direction
         record.object    = nearest_object(record.position)
         record.distance += record.object.distance
-        record.hit       = record.object.distance < PRECISION
-        if record.distance > MAX_DIS or record.hit: break
+        record.hit       = record.object.distance < 0.0001
+        if record.distance > 2000.0 or record.hit: break
     return record
 
 @ti.func
@@ -98,15 +86,10 @@ def hemispheric_sampling(normal: vec3) -> vec3:
     return normalize(normal + vec3(xy, z))
 
 @ti.func
-def brightness(rgb: vec3) -> float:
-    return dot(rgb, vec3(0.299, 0.587, 0.114))
-
-@ti.func
 def raytrace(ray: Ray) -> Ray:
-    for i in range(MAX_RAYTRACE):
-        inv_pdf = exp(float(i) / light_quality)
+    for i in range(3):
+        inv_pdf = exp(float(i) / 128.0)
         roulette_prob = 1.0 - (1.0 / inv_pdf)
-        
         if ti.random() < roulette_prob: ray.color *= roulette_prob; break
 
         record = raycast(ray)
@@ -114,16 +97,13 @@ def raytrace(ray: Ray) -> Ray:
 
         normal  = calc_normal(record.object, record.position)
         ray.direction = hemispheric_sampling(normal)
-
         ray.color *= record.object.material.albedo
         ray.origin = record.position
 
-        intensity  = brightness(ray.color)
+        intensity  = dot(ray.color, vec3(0.299, 0.587, 0.114))
         ray.color *= record.object.material.emission
-        visible    = brightness(ray.color)
-
-        if intensity < visible or visible < VISIBILITY: break
-
+        visible    = dot(ray.color, vec3(0.299, 0.587, 0.114))
+        if intensity < visible or visible < 0.000001: break
     return ray
 
 @ti.kernel
@@ -131,50 +111,39 @@ def render(camera_position: vec3, camera_lookat: vec3, camera_up: vec3):
     for i, j in image_pixels:
         buffer = image_buffer[i, j]
 
-        for _ in range(SAMPLE_PER_PIXEL):
-            coord = vec2(i, j) + vec2(ti.random(), ti.random())
-            uv = coord * SCREEN_PIXEL_SIZE
+        half_height = tan(radians(35) * 0.5)
+        half_width = aspect_ratio * half_height
 
-            theta = radians(camera_vfov)
-            half_height = tan(theta * 0.5)
-            half_width = aspect_ratio * half_height
+        z = normalize(camera_position - camera_lookat)
+        x = normalize(cross(camera_up, z))
+        y = cross(z, x)
+        
+        hwfx = half_width  * x
+        hhfy = half_height * y
+        lower_left_corner = camera_position - hwfx - hhfy - z
+        horizontal = 2.0 * hwfx
+        vertical   = 2.0 * hhfy
 
-            z = normalize(camera_position - camera_lookat)
-            x = normalize(cross(camera_up, z))
-            y = cross(z, x)
-            
-            hwfx = half_width  * x
-            hhfy = half_height * y
+        uv = (vec2(i, j) + vec2(ti.random(), ti.random())) * SCREEN_PIXEL_SIZE
+        ro = camera_position
+        po = lower_left_corner + uv.x * horizontal + uv.y * vertical
+        rd = normalize(po - ro)
 
-            lower_left_corner = camera_position - hwfx - hhfy - z
-            horizontal = 2.0 * hwfx
-            vertical   = 2.0 * hhfy
+        ray = raytrace(Ray(ro, rd, vec3(1)))
+        buffer += vec4(ray.color, 1.0)
+        image_buffer[i, j] = buffer
 
-            ro = camera_position
-            po = lower_left_corner + uv.x * horizontal + uv.y * vertical
-            rd = normalize(po - ro)
-
-            ray = raytrace(Ray(ro, rd, vec3(1)))
-            buffer += vec4(ray.color, 1.0)
-
-        color  = buffer.rgb / buffer.a
-        color *= camera_exposure
+        color = buffer.rgb / buffer.a
         color = mat3(0.59719, 0.35458, 0.04823, 0.07600, 0.90834, 0.01566, 0.02840, 0.13383, 0.83777)  @ color
         color = (color * (color + 0.0245786) - 0.000090537) / (color * (0.983729 * color + 0.4329510) + 0.238081)
         color = mat3(1.60475, -0.53108, -0.07367, -0.10208, 1.10813, -0.00605, -0.00327, -0.07276, 1.07602) @ color
-        color = pow(color, vec3(1.0 / camera_gamma))
-
-        image_buffer[i, j] = buffer
+        color = pow(color, vec3(1.0 / 2.2))
         image_pixels[i, j] = color
 
 window = ti.ui.Window("Cornell Box", image_resolution)
 canvas = window.get_canvas()
-camera = ti.ui.Camera()
-camera.position(0, 0, 3.5*10)
 
 while window.running:
-    camera.track_user_inputs(window, movement_speed=0.3, hold_key=ti.ui.LMB)
-    render(camera.curr_position, camera.curr_lookat, camera.curr_up)
+    render(vec3(0, 0, 3.5), vec3(0, 0, -1), vec3(0, 1, 0))
     canvas.set_image(image_pixels)
-
     window.show()
