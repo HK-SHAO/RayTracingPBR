8a9a2d6dc7157aa321a3f5cbb9238a325a6019fd
HK-SHAO <hk-shao@foxmail.com>
2023-01-29 17:05:50 +0800

    format codes

--- examples/cornell_box/cornell_box_shortest.py
+++ examples/cornell_box/cornell_box_shortest.py
@@ -125,8 +125,8 @@ def render(camera_position: vec3, camera_lookat: vec3, camera_up: vec3):
 
         color = buffer.rgb / buffer.a                                  # calculate the average value of colors
         color = pow(color, vec3(1.0 / 2.2))                     # Gamma correction, then use ACES tone mapping
-        color = mat3(0.59719, 0.35458, 0.04823, 0.07600, 0.90834, 0.01566, 0.02840, 0.13383, 0.83777)  @ color
-        color = (color * (color + 0.0245) - 0.000090537) / (color * (0.983 * color + 0.4329510) + 0.238081)
+        color = mat3(0.597190, 0.35458, 0.04823, 0.07600, 0.90834, 0.01566, 0.02840, 0.13383, 0.83777) @ color
+        color = (color * (color + 0.024578) - 0.0000905) / (color * (0.983729 * color + 0.4329510) + 0.238081)
         color = mat3(1.60475, -0.531, -0.0736, -0.102, 1.10813, -0.00605, -0.00327, -0.07276, 1.07602) @ color
         image_pixels[i, j] = clamp(color, 0, 1)  # write pixels, clamp the brightness that cannot be displayed
 
